189b5721d4fc0d7e5d06a8df3280e28fe04c382e
Kirill Lobunko <k.lobunko@gmail.com>
2020-01-28 11:18:13 +0700

    hw kubernetes-templating commit

---
+++ kubernetes-templating/repo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+##helm repo add templating https://harbor.35.228.204.248.nip.io/chartrepo/library
+helm repo add templating https://harbor.35.228.204.248.nip.io/chartrepo/library
+helm repo add templating https://harbor.35.228.204.248.nip.io/chartrepo/mychartrepo
